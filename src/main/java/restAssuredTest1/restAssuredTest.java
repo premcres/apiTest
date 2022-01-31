d28add4bb9f1ae207c5ad96e7a079e9b6a942be7
premcrescent <premcrescent@gmail.com>
2022-01-31 14:31:59 +0530

    updates

--- src/main/java/restAssuredTest1/restAssuredTest.java
+++ src/main/java/restAssuredTest1/restAssuredTest.java
@@ -128,7 +128,7 @@ public class restAssuredTest {
 	}
 
 	
-	@Test(invocationCount = 100)
+	@Test(invocationCount = 300)
 	public void getResposeData_10()
 	{
 	Response resp= RestAssured.get("http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02");
